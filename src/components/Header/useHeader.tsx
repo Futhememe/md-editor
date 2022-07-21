6d8c3dbce4104f6efd4c75ebb77209345566edbc
Futhememe <kyoro_souza@hotmail.com>
2022-07-20 21:17:17 -0300

    ✨ new dowload function

--- src/components/Header/useHeader.tsx
+++ src/components/Header/useHeader.tsx
@@ -13,6 +13,22 @@ export const useHeader = () => {
   const normalizeDocumentName = (text: string): string =>
     text.includes(".md") ? text : text.concat(".md");
 
+  const downloadFile = (filename: string, text: string) => {
+    let element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+    );
+    element.setAttribute("download", filename);
+
+    element.style.display = "none";
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  };
+
   return {
     isBase,
     colorMode,
