c798a2629db8925409a840117ff3a405d9683a57
Futhememe <kyoro_souza@hotmail.com>
2022-07-29 12:13:17 -0300

    💄 new background to match with editor

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,7 +3,7 @@ import { Flex, useColorMode, FlexProps } from "@chakra-ui/react";
 export const Container = (props: FlexProps) => {
   const { colorMode } = useColorMode();
 
-  const bgColor = { light: "white", dark: "#1A1A1A" };
+  const bgColor = { light: "white", dark: "#1E1E1E" };
   const color = { light: "black", dark: "white" };
 
   return (
